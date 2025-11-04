30988f5ba05cce2c3368488ea5c5c9abf552e09e
wwzzz <zwang@stu.xmu.edu.cn>
2025-11-04 11:26:37 +0800

    update training config at 1104

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -707,8 +707,29 @@ class TrainConfig:
             raise ValueError("Cannot resume and overwrite at the same time.")
 
 
-# Use `get_config` if you need to get a config by name in your code.
+# Use `get_config` if you need to get a config by name in your 
 _CONFIGS = [
+    TrainConfig(
+        name="pi05_place1103",
+        model=pi0_config.Pi0Config(pi05=True, action_horizon=100, discrete_state_input=False, async_action_horizon=1),
+        data=LeRobotAsyncBiFlexivDataConfig(
+            repo_id="flexiv/pick_1103_debug", # change name here
+            base_config=DataConfig(prompt_from_task=False),
+            extra_delta_transform=True,
+        ),
+        batch_size=32,
+        lr_schedule=_optimizer.CosineDecaySchedule(
+            warmup_steps=10_000,
+            peak_lr=5e-5,
+            decay_steps=1_000_000,
+            decay_lr=5e-5,
+        ),
+        optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
+        ema_decay=0.999,
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
+        pytorch_weight_path="/path/to/your/pytorch_weight_path",
+        num_train_steps=30_000,
+    ),
     TrainConfig(
         name="pi05_place1102",
         model=pi0_config.Pi0Config(pi05=True, action_horizon=100, discrete_state_input=False, async_action_horizon=1),
@@ -717,7 +738,7 @@ _CONFIGS = [
             base_config=DataConfig(prompt_from_task=False),
             extra_delta_transform=True,
         ),
-        batch_size=16,
+        batch_size=32,
         lr_schedule=_optimizer.CosineDecaySchedule(
             warmup_steps=10_000,
             peak_lr=5e-5,
