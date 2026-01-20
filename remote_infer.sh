50bf943f79ca01e0b19684f3ef14f915a7a46b19
Wendi Chen <chenwendi-andy@sjtu.edu.cn>
2026-01-20 12:57:37 +0800

    [feat] add remote infer script

---
+++ remote_infer.sh
@@ -0,0 +1,3 @@
+uv run scripts/serve_policy.py policy:checkpoint \
+  --policy.config=pi05_iPhoneSingle_hardtowel_660 \
+  --policy.dir=/home/wendi/Desktop/openpi/checkpoints/pi05_iPhoneSingle_hardtowel_660/hardtowel_660_run1/15000
