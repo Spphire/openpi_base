de01d3d24557b0ddd9104838f7cbbb965a1220e8
Wendi Chen <chenwendi-andy@sjtu.edu.cn>
2026-01-20 14:54:40 +0800

    [fix] potential import issue

--- src/openpi/training/online_data_fetcher.py
+++ src/openpi/training/online_data_fetcher.py
@@ -5,7 +5,7 @@ Author: Wendi Chen
 """
 import os
 import sys
-sys.path.append(os.getcwd())
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 
 import hashlib
 import logging
